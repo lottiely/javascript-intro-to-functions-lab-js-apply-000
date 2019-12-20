27ae2bbd9ae6f27d9726a1b7a9be659fedacbbd6
Shanan Almario <saialmario1@gmail.com>
2019-12-20 20:28:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 let shout = string => string.toUpperCase();
 let whisper = string => string.toLowerCase();
+let logShout = string => console.log(string.toUpperCase());
+let logWhisper = string => console.log(string.toLowerCase());
+let sayHiToGrandma = string => {
+  if(string == string.toLowerCase())
+    return "I can't hear you!"
+}
